44c1c35a7f675ee90a15e0436f30f88a13b9c54e
cestrmail@163.com <cestrmail@163.com>
2022-08-10 18:21:48 +0800

    配置 element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: cest
+ * @Date: 2022-08-10 08:58:17
+ * @LastEditTime: 2022-08-10 12:50:13
+ * @LastEditors: cest
+ * @FilePath: /vue3-vite3-todo-list/vite.config.ts
+ * @Description: 编辑描述内容
+ */
 /// <reference types="vitest" />
 
 import path from 'path'
@@ -7,6 +15,8 @@ import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
+
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   resolve: {
@@ -35,11 +45,13 @@ export default defineConfig({
         './src/composables',
       ],
       vueTemplate: true,
+      resolvers: [ElementPlusResolver()],
     }),
 
     // https://github.com/antfu/vite-plugin-components
     Components({
       dts: true,
+      resolvers: [ElementPlusResolver()],
     }),
 
     // https://github.com/antfu/unocss
