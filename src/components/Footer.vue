f73b57c2454d0ccb2787795083bc6845fd9ae7de
cestrmail@163.com <cestrmail@163.com>
2022-08-10 18:24:20 +0800

    Footer 组件：新增返回首页按钮

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,20 @@
+<!--
+ * @Author: cest
+ * @Date: 2022-08-10 08:58:17
+ * @LastEditTime: 2022-08-10 15:00:44
+ * @LastEditors: cest
+ * @FilePath: /vue3-vite3-todo-list/src/components/Footer.vue
+ * @Description: 编辑描述内容
+-->
 <template>
   <nav text-xl mt-6 inline-flex gap-2>
+    <a
+      icon-btn i-carbon-campsite
+      rel="noreferrer"
+      href="/"
+      title="GitHub"
+    />
+
     <button icon-btn @click="toggleDark()">
       <div dark:i-carbon-moon i-carbon-sun />
     </button>
