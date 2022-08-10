68408f463fbef01fbf6c00028733b29610fb7e2f
cestrmail@163.com <cestrmail@163.com>
2022-08-10 10:47:55 +0800

    测试网路字体

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
+<!--
+ * @Author: cest
+ * @Date: 2022-08-10 08:58:17
+ * @LastEditTime: 2022-08-10 10:08:46
+ * @LastEditors: cest
+ * @FilePath: /vue3-vite3-todo-list/src/App.vue
+ * @Description: 编辑描述内容
+-->
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <main font-mono p="x-4 y-10" text="center gray-700 dark:gray-200">
     <router-view />
     <Footer />
   </main>
