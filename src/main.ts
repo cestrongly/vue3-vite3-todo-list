862e43d63504e0175aecf2429c86ed8b03f17bd8
cestrmail@163.com <cestrmail@163.com>
2022-08-10 10:48:07 +0800

    测试网路字体

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: cest
+ * @Date: 2022-08-10 08:58:17
+ * @LastEditTime: 2022-08-10 10:11:06
+ * @LastEditors: cest
+ * @FilePath: /vue3-vite3-todo-list/src/main.ts
+ * @Description: 编辑描述内容
+ */
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
@@ -6,6 +14,8 @@ import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+import './assets/fonts/dm/index.css'
+import './assets/styls/index.styl'
 
 const app = createApp(App)
 const router = createRouter({
